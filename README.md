704fc243e3ed707001c08fb40c0be751b9946c23
paolohera <143242348+paolohera@users.noreply.github.com>
2024-05-11 13:39:52 +0800

    Add files via upload
    npm install
    npm express install socket.io

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# newCotton
+# Team_Cotton
+IPT Socket lesson <br>
+Members: <br>
+Troy Saberon <br>
+Christopher Job DilaO <br>
+Paolo Patigdas
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require ('http');
+const socketIo = require ('socket.io');  
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors:{
+        origin: "*",
+        methods: "*"
+    }
+}
+);
+
+io.on('connection', (socket) =>{
+
+    socket.on('message', (message) =>{
+        io.emit('message', message);
+    });
+});
+
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
